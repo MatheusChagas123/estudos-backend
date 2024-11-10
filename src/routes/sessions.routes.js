427600ffee3a0d08d4ca39f68ed41218a6085f5b
Matheus Chagas <matheuschagas29@gmail.com>
2024-11-10 13:49:52 -0300

    criando rotas de autenticação

---
+++ src/routes/sessions.routes.js
@@ -0,0 +1,9 @@
+const {Router} = require ("express");
+
+const SessionsController =require("../controllers/SessionsController");
+const sessionsController = new SessionsController(); 
+
+const sesseionRoutes = Router();
+sesseionRoutes.post("/",sessionsController.create)
+module.exports = sesseionRoutes;
+
