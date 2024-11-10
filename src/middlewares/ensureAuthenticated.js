5bea30d0b8898fb53180961bb8d524f76872b01b
Matheus Chagas <matheuschagas29@gmail.com>
2024-11-10 14:54:44 -0300

    create middleware de autenticação

---
+++ src/middlewares/ensureAuthenticated.js
@@ -0,0 +1,24 @@
+const {verify} = require("jsonwebtoken");
+const AppError = require("../utils/AppError");
+const authConfig = require("../configs/auth");
+
+function ensureAuthenticated(request, response,next){
+const autHeader = request.headers.authorization;
+if(!autHeader){
+  throw new AppError("JWT token não informado",401);
+}
+const [,token] = autHeader.split(" ");
+
+try {
+  const {sub:user_id} = verify(token,AuthConfig.jwt.secret);
+  request.user ={
+    id: Number(user_id)
+  };
+
+  return next();
+}catch{
+  throw new AppError("JWT token inválido",401);
+}
+}
+
+module.export = ensureAuthenticated;
